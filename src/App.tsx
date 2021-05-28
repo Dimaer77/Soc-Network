6daa0c9e8018de07942863ac51332d03644c5af9
Dimaer77 <34601702+Dimaer77@users.noreply.github.com>
2021-05-28 14:50:14 +0300

    L11-grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Technologies} from "./components/Hechnoliges";
+import {Header} from "./components/Header";
+import {Footer} from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"wrapper-content"}>
+            <header className={"header"}>
+                <img style={{width: "50px", height: "100%", marginLeft: "5px"}}
+                     src="https://icon-library.net/images/icon-png-logos/icon-png-logos-9.jpg" alt=""/>
+            </header>
+            <nav className={"nav"}>
+                <ul>
+                    <li><a>Profile</a></li>
+                    <li><a>Messages</a></li>
+                    <li><a>News</a></li>
+                    <li><a>Music</a></li>
+                    <li><a>Setting</a></li>
+                </ul>
+            </nav>
+            <div className={"content"}>
+                <div>
+                    <img src="https://i.ytimg.com/vi/xrdVMaUaaYQ/maxresdefault.jpg" alt="img"/>
+                </div>
+                <div>
+                    <img style={{width: "80px", height: "80px", marginLeft: "5px"}}
+                         src="https://s2.desu-usergeneratedcontent.xyz/g/image/1504/46/1504460092425.jpg" alt=""/>
+                    description
+                </div>
+                <div>
+                    My Pos ts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
